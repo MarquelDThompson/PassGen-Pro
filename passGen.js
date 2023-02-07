4a0a88cc6c967d54d1f44dc127cfb70ddfdfd345
MarquelDThompson <marquelt3@gmail.com>
2023-02-07 12:41:27 -0800

    first commit

---
+++ passGen.js
@@ -0,0 +1,35 @@
+const passwordLengthInput = document.querySelector('#password-length');
+const includeNumbersInput = document.querySelector('#include-numbers');
+const includeSymbolsInput = document.querySelector('#include-symbols');
+const includeLettersInput = document.querySelector('#include-letters');
+const passwordDisplay = document.querySelector('#password');
+const form = document.querySelector('form');
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+-=[]{}|;:\'",.<>/?\\';
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let password = '';
+  let passwordPool = '';
+  
+  if (includeLettersInput.checked) {
+    passwordPool += letters;
+  }
+  
+  if (includeNumbersInput.checked) {
+    passwordPool += numbers;
+  }
+  
+  if (includeSymbolsInput.checked) {
+    passwordPool += symbols;
+  }
+  
+  for (let i = 0; i < passwordLengthInput.value; i++) {
+    password += passwordPool.charAt(Math.floor(Math.random() * passwordPool.length));
+  }
+  
+  passwordDisplay.textContent = password;
+});
